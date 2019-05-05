49f81ebc34f51755168989df39359524cbef35f9
RomanSlep <slep48@gmail.com>
2019-05-05 12:51:14 +0300

    add register and login

--- server/modules/api.js
+++ server/modules/api.js
@@ -1,31 +1,87 @@
 const log = require('../helpers/log');
-const {
-    usersDb
-} = require('./DB');
+const sha256 = require('sha256');
+const {usersDb} = require('./DB');
 
 module.exports = (app) => {
-    app.get('/api', (req, res) => {
+    app.get('/api', async (req, res) => {
         try {
-            const GET = req.query;
-            success(null, res);
-            usersDb.insert(GET); // save in DB
+            const action = req.query.action.toLowerCase();
+            const GET = JSON.parse(req.query.data);
+            // роуты
+            switch (action) {
+            case ('login'):
+                usersDb.findOne({$and: [{login: GET.login}, {password: GET.password}]}, (err, user)=>{
+                    if (!user){
+                        error('This login and password not found', res);
+                        return;
+                    }
+                    success(assignUser(user), res);
+                });
+                break;
+                
+            case ('registration'):
+                const { login, password, address } = GET;
+                if (!login.length || !password.length || !address.length) {
+                    error('No full data', res);
+                    return;
+                }
+                const checUser = await usersDb.syncFindOne({
+                    $or: [{ address }, { login }]
+                });
+               
+                if (checUser){
+                    error('Login or password already exists!', res);
+                    return;
+                }
+                usersDb.insert({
+                    address,
+                    login,
+                    password
+                }, (err, newUser) => {
+                    if (err){
+                        error('Error create user', res);
+                        return;
+                    }
+                    success(assignUser(newUser), res);
+                });
+                break;
+            }
         } catch (e) {
-            error(e, res);
+            console.log({e});
+            error('Error api code 1', res);
         }
     });
 };
 
-function error (err, res) {
-    log.error(err);
-    res.json({
-        success: false,
-        error: err
-    });
+function error(msg, res) {
+    try {
+        log.error(msg);
+        res.json({
+            success: false,
+            msg,
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+function success(data, res) {
+    try {
+        res.json({
+            success: true,
+            result: data
+        });
+    } catch (e) {
+        console.log(e);
+    }
 }
 
-function success (data, res) {
-    res.json({
-        success: true,
-        result: data
-    });
+function assignUser(user){
+    try {
+        user.token = sha256(new Date().toString());
+        delete user._id;
+        delete user.password;
+        return user;
+    } catch (e){
+        console.log('assignUser: ' + e);
+    }
 }
