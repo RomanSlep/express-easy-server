91b3fbfbcc030b734c890d2a387abc14277e31da
Roman <slep48@gmail.com>
2019-05-07 02:18:34 +0300

    add multirandomiser

--- server/modules/startGame.js
+++ server/modules/startGame.js
@@ -16,7 +16,13 @@ module.exports = async (User, params) => {
         msg = 'No valid count cells!';
     }
     //TODO: Проверить что есть незаконченный матч
-    const noFinished = await gamesDb.syncFindOne({$and: [{user_id: User._id}, {isGame: true}]});
+    const noFinished = await gamesDb.syncFindOne({
+        $and: [{
+            user_id: User._id
+        }, {
+            isGame: true
+        }]
+    });
     if (noFinished) {
         msg = 'You have no finished game!';
     }
@@ -46,7 +52,8 @@ module.exports = async (User, params) => {
         steps: {},
         stepLastNum: 0, // номер последнего шага по очередности нажатия
         lastCell: 0, // номер последнего шага по очередности нажатия
-        isGame: true
+        isGame: true,
+        unixStart: new Date().getTime()
     };
 
     $u.prizes(game);
@@ -58,13 +65,59 @@ module.exports = async (User, params) => {
     };
 };
 
-function createBombs(count) {
-    const arrBombs = [];
+function createBombs(countBombs) {
+    let count = countBombs;
+    let isCurrent = true;
+    if (countBombs > 14) {
+        isCurrent = false;
+        count = 25 - countBombs;
+    }
+    let arrBombs = [];
+    const rndArr = getRndArray(25);
     while (arrBombs.length < count) {
-        const pos = +(Math.random() * 24).toFixed(0);
-        if (!arrBombs.includes(pos)) {
-            arrBombs.push(pos);
+        let posLow = rndArr.shift();
+        let posTop = posLow;
+        const length = arrBombs.length;
+        while (arrBombs.length === length) {
+            if (!arrBombs.includes(posTop)) {
+                arrBombs.push(posTop);
+                continue;
+            }
+            if (!arrBombs.includes(posLow)) {
+                arrBombs.push(posLow);
+            }
+            posTop++;
+            posLow--;
         }
-    };
+    }
+
+    if (!isCurrent) { // обернуть массив
+        const invertArray = [];
+        for (let c = 0; c < 25; c++) {
+            if (!arrBombs.includes(c)) {
+                invertArray.push(c);
+            }
+        }
+        arrBombs = invertArray;
+    }
+    // while (arrBombs.length < count) {
+    //     const pos = +(Math.random() * 24).toFixed(0);
+    //     if (!arrBombs.includes(pos)) {
+    //         arrBombs.push(pos);
+    //     }
+    // };
     return arrBombs;
+}
+
+function getRndArray(max) {
+    const rnd = Math.round(Math.random() * 100).toFixed(0); // Пришедшее из рандомайзера от 0 до 100
+    const randoms = String(rnd / new Date().getTime() * 10000000000000000).replace('.', '');
+    let i = 0;
+    const arr = [];
+    while (randoms[i + 1]) {
+        const rnd = Math.round((randoms[i] + randoms[i + 1]) / (100 / max));
+        arr.push(rnd);
+        i++;
+    }
+    return arr;
 }
